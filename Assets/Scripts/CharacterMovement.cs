8f9f0e066a66c812e86b75f0eba3800f052b674d
harry1878 <kwakju1878@gmail.com>
2020-04-04 18:05:24 +0900

    2D_RPG

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public Animator animator = null;
+    public Rigidbody2D rigid = null;
+    public float speed = 2f;
+    public float jumpPower = 10f;
+
+    private bool isMove = false;
+    private bool isReverse = false;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            Move(false);
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            Move(true);
+        }
+        else
+        {
+            isMove = false;
+        }
+        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(!animator.GetBool("isJump"))
+            {
+                rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+                animator.SetBool("isJump", true);
+                animator.SetBool("isJumpDown", false);
+            }
+        }
+
+        animator.SetBool("isMove", isMove);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isMove)
+            transform.Translate(speed * Time.fixedDeltaTime, 0, 0);
+        
+        if(animator.GetBool("isJump"))
+        {
+            if(rigid.velocity.y <0)
+            {
+                animator.SetBool("isJumpDown", true);
+            }
+        }
+    }
+
+    private void Move(bool isLeft)
+    {
+        isMove = true;
+
+        if(isReverse != isLeft)
+        {
+            //삼항 연산자 
+            // isLeft ? -> if(isLeft == true)
+            // !isLeft ? -> if(isLeft != true)
+            transform.localRotation = isLeft ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+
+            isReverse = isLeft;
+        }
+
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        animator.SetBool("isJump", false);
+    }
+    private void Jump()
+    {
+        
+    }
+}
